db317f01070d1657377d65effa584d6c78f369f0
Tigris <tabula.tenebris@gmail.com>
2021-09-19 05:34:59 +0600

    Add project files.

---
+++ ConstData/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstData
+{
+    class MyMathClass
+    {
+        //public const double PI = 3.14;
+        public static readonly double PI=3.14;
+        //public MyMathClass()
+        //{
+        //    PI = 3.14;
+        //}
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"The value of PI is {MyMathClass.PI}");
+            //Error: MyMathClass.PI = 3.1444;
+            Console.WriteLine();
+        }
+        static void LocalConstStringVariable()
+        {
+            const string fixedStr = "Fixed string Data";
+            Console.WriteLine(fixedStr);
+        }
+    }
+}
